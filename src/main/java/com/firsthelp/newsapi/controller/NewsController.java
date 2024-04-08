477604d12a0682a259924b802911b1c58b1c62fa
Nathan Ramdial <nathan.ramdial@gmail.com>
2024-04-07 21:02:43 -0400

    Updated OpenAPI spec documentation for /search endpoint

--- src/main/java/com/firsthelp/newsapi/controller/NewsController.java
+++ src/main/java/com/firsthelp/newsapi/controller/NewsController.java
@@ -2,10 +2,45 @@ package com.firsthelp.newsapi.controller;
 
 import com.firsthelp.newsapi.SearchFilter;
 import com.firsthelp.newsapi.dto.ArticleResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface NewsController {
-  ResponseEntity<ArticleResponseDto> getArticles(Integer limit, String keywords, List<SearchFilter> filter);
+
+  @Operation(summary = "Get a list of articles")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Found a list of articles",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ArticleResponseDto.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+        @ApiResponse(responseCode = "404", description = "No articles found", content = @Content)
+      })
+  ResponseEntity<ArticleResponseDto> getArticles(
+      @Valid
+          @RequestParam(required = false, defaultValue = "10")
+          @Parameter(description = "Number of articles to return")
+          @Min(1)
+          @Max(100)
+          Integer limit,
+      @RequestParam @Parameter(description = "Required keyword to search by") String keyword,
+      @RequestParam(name = "filter", required = false, defaultValue = "")
+          @Parameter(description = "Comma-separated filter categories (e.g. \"title,content\", \"content\" ")
+          List<SearchFilter> filter);
 }
--- src/main/java/com/firsthelp/newsapi/controller/NewsControllerImpl.java
+++ src/main/java/com/firsthelp/newsapi/controller/NewsControllerImpl.java
@@ -6,12 +6,13 @@ import com.firsthelp.newsapi.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class NewsControllerImpl implements NewsController {
 
@@ -20,9 +21,7 @@ public class NewsControllerImpl implements NewsController {
   @Override
   @GetMapping("/articles")
   public ResponseEntity<ArticleResponseDto> getArticles(
-      @RequestParam(required = false, defaultValue = "10") Integer limit,
-      @RequestParam String keyword,
-      @RequestParam(name = "filter", required = false, defaultValue = "") List<SearchFilter> filter) {
+      Integer limit, String keyword, List<SearchFilter> filter) {
     String criteriaString = filter.toString();
     ArticleResponseDto response = articleService.searchArticles(limit, keyword, criteriaString);
     return new ResponseEntity<>(response, HttpStatus.OK);
